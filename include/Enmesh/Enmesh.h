057096c0dbc75e795dff5084ca22da60a750d470
Alexis Laurent <alexis.laurent1321@gmail.com>
2026-05-02 21:55:38 +0200

    Remove Enmesh namespace from header file and adjust include statements

--- include/Enmesh/Enmesh.h
+++ include/Enmesh/Enmesh.h
@@ -1,22 +1,19 @@
 #ifndef ENMESH_H
 #define ENMESH_H
 
-namespace Enmesh {
-    
-    #include "geometry/edge.h"
-    #include "geometry/point.h"
-    #include "geometry/triangle.h"
-    #include "geometry/tetrahedron.h"
+#include "geometry/edge.h"
+#include "geometry/point.h"
+#include "geometry/triangle.h"
+#include "geometry/tetrahedron.h"
 
-    #include "io/exportToVTK.h"
-    #include "io/loadMsh.h"
-    #include "io/loadObj.h"
+#include "io/exportToVTK.h"
+#include "io/loadMsh.h"
+#include "io/loadObj.h"
 
-    #include "mesh/mesh.h"
-    #include "mesh/mesh_analysis.h"
-    #include "mesh/mesh_basic_shapes.h"
-    #include "mesh/mesh_smoothing.h"
-    #include "mesh/mesh_triangulation.h"
-}
+#include "mesh/mesh.h"
+#include "mesh/mesh_analysis.h"
+#include "mesh/mesh_basic_shapes.h"
+#include "mesh/mesh_smoothing.h"
+#include "mesh/mesh_triangulation.h"
 
 #endif  
