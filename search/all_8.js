899d54ac5ae0d4081438826017b363771009ea14
alexislaurent1321-gif <alexis.laurent1321@gmail.com>
2026-06-07 08:27:07 +0000

    Deploying to gh-pages from @ alexislaurent1321-gif/Enmesh@a8403814288c0b3df3b27a5b69a7d6f28c68ceb8 🚀

--- search/all_8.js
+++ search/all_8.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['generate_20a_20mesh_20demo_20demo_5fgenerategrid_20cpp_0',['Generate a mesh (&lt;span class=&quot;tt&quot;&gt;demo/demo_generateGrid.cpp&lt;/span&gt;)',['../index.html#autotoc_md9',1,'']]],
-  ['getfaces_1',['getFaces',['../structEnmesh_1_1Hexa.html#a927b2824f85ef5e9596fc3cca8b7bc6a',1,'Enmesh::Hexa']]],
+  ['getfaces_1',['getFaces',['../structEnmesh_1_1Hexa.html#a927b2824f85ef5e9596fc3cca8b7bc6a',1,'Enmesh::Hexa::getFaces()'],['../structEnmesh_1_1Tetra.html#a3bc19e050e7f595c9148722ac29c675b',1,'Enmesh::Tetra::getFaces()']]],
   ['getnumelements_2',['getNumElements',['../structEnmesh_1_1Mesh.html#aebdc7a5c1f91ae61c9e0214da7da44e4',1,'Enmesh::Mesh']]],
   ['getnumvertices_3',['getNumVertices',['../structEnmesh_1_1Mesh.html#ac8ce69ed858b170afb6a2927a0e392b4',1,'Enmesh::Mesh']]],
   ['grid_4',['Test on a regular grid',['../index.html#autotoc_md11',1,'']]]
